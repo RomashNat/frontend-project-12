c94e7d56a31737485171e19469b6e1bb32114449
Natalia Romashev <tatasay@mail.ru>
2025-11-10 00:29:33 +0300

    Changed socket

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,51 @@
 import { io } from 'socket.io-client'
 
-const socket = io()
+const socket = io('', {
+  path: '/socket.io',
+  autoConnect: false,
+  auth: {
+    token: localStorage.getItem('token'),
+  },
+})
 
-export default socket
+// Функции для управления сокетом
+export const connectSocket = () => {
+  const token = localStorage.getItem('token');
+  if (token && !socket.connected) {
+    socket.auth.token = token;
+    socket.connect();
+  }
+}
+
+export const disconnectSocket = () => {
+  if (socket.connected) {
+    socket.disconnect();
+  }
+}
+
+export const onNewMessage = (callback) => {
+  socket.on('newMessage', callback);
+}
+
+export const removeMessageListener = () => {
+  socket.off('newMessage');
+}
+
+export const joinChannel = (channelId) => {
+  if (socket.connected) {
+    socket.emit('joinChannel', channelId);
+  }
+}
+
+export const leaveChannel = (channelId) => {
+  if (socket.connected) {
+    socket.emit('leaveChannel', channelId);
+  }
+}
+
+// Автоподключение если есть токен
+if (localStorage.getItem('token')) {
+  connectSocket();
+}
+
+export default socket;
